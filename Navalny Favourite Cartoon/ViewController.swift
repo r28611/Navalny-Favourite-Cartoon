8052f3b4942f860b4d986aacafa89d74d53b0b63
Margarita Novokh <r28611@gmail.com>
2022-04-25 23:38:37 +0200

    Initial Commit

---
+++ Navalny Favourite Cartoon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Navalny Favourite Cartoon
+//
+//  Created by Margarita Novokhatskaia on 25/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
