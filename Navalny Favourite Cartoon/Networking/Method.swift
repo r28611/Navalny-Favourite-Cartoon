83325ac2ef297b21a62151226220cf0e35fa5a31
Margarita Novokh <r28611@gmail.com>
2022-04-26 16:00:12 +0200

    added api methods

--- Navalny Favourite Cartoon/Networking/Method.swift
+++ Navalny Favourite Cartoon/Networking/Method.swift
@@ -11,16 +11,17 @@ enum Method {
     static let baseURL = URL(string: "https://rickandmortyapi.com/api/")!
 
     case character(Int)
-    case location
-    case episode
+    case location(Int)
+    case episode(Int)
 
     var url: URL {
         switch self {
         case .character(let id):
             return Method.baseURL.appendingPathComponent("character/\(id)")
-        default:
-            // Homework
-            fatalError("URL for this case is undefined.")
+        case .location(let id):
+            return Method.baseURL.appendingPathComponent("location/\(id)")
+        case .episode(let id):
+            return Method.baseURL.appendingPathComponent("episode/\(id)")
         }
     }
 }
