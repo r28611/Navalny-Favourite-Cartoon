8a8e0ddb65cf84c40f8e0aa47de15900ca9f6d34
Margarita Novokh <r28611@gmail.com>
2022-04-28 19:30:24 +0200

    added models: episode, location; ui for episode cell, location cell

---
+++ Navalny Favourite Cartoon/Model/Location.swift
@@ -0,0 +1,15 @@
+//
+//  Location.swift
+//  Navalny Favourite Cartoon
+//
+//  Created by Margarita Novokhatskaia on 28/04/2022.
+//
+
+import Foundation
+
+struct LocationData: Codable {
+    let id: Int
+    let name: String
+    let type: String
+    let residents: [String]
+}
--- Navalny Favourite Cartoon/View/LocationsTableViewController.swift
+++ Navalny Favourite Cartoon/View/LocationsTableViewController.swift
@@ -7,6 +7,31 @@
 
 import UIKit
 
+private let reuseIdentifier = "Cell"
+
 final class LocationsTableViewController: UITableViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView!.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
 }
